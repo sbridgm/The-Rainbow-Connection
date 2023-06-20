aa87fdd19469ddabc1aec27f5f179fff4db94697
sbridgm <136629639+sbridgm@users.noreply.github.com>
2023-06-20 14:45:07 +0700

    Fix memory management issues

---
+++ sun_jungle_king.pl
@@ -0,0 +1,50 @@
+#!/usr/bin/perl
+
+# Playing the song
+print "The Rainbow Connection\n\n";
+print "Why are there so many\n";
+print "Songs about rainbows\n";
+print "And what's on the other side\n";
+print "Rainbows are visions\n";
+print "But only illusions\n";
+print "And rainbows have nothing to hide\n\n";
+
+# Singers
+print "So we've been told\n";
+print "And some choose to believe it\n";
+print "I know they're wrong, wait and see\n";
+print "Someday we'll find it\n";
+print "The rainbow connection\n";
+print "The lovers, the dreamers, and me\n\n";
+
+# Who said it?
+print "Who said the world\n";
+print "Was ever gonna be\n";
+print "Sunshine and rainbows\n";
+print "It's not always what it seems\n";
+print "That's all part of the scheme\n";
+print "I know someday we'll find it\n";
+print "The rainbow connection\n";
+print "The lovers, the dreamers, and me\n\n";
+
+# Aim higher
+print "La la la la la\n";
+print "La la la la lee\n";
+print "La la la la la\n";
+print "We'll be following the beam\n";
+print "We'll find a new fantasy\n";
+print "And that's our destiny\n";
+print "We know someday we'll find it\n";
+print "The rainbow connection\n";
+print "The lovers, the dreamers, and me\n\n";
+
+# Finale 
+print "La la la la la\n";
+print "La la la la lee\n";
+print "La la la la la\n";
+print "We'll be following the beam\n";
+print "We'll find a new way of living\n";
+print "And then we'll start forgiving\n";
+print "We know someday we'll find it\n";
+print "The rainbow connection\n";
+print "The lovers, the dreamers, and me\n";
