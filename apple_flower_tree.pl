b22e03f0378af427383e311e96d1513fbb3399b9
sbridgm <136629639+sbridgm@users.noreply.github.com>
2023-06-26 21:15:09 +0700

    Add logging

---
+++ apple_flower_tree.pl
@@ -0,0 +1,242 @@
+#!/usr/bin/perl
+
+# 'The Rainbow Connection'
+ 
+use strict;
+use warnings;
+ 
+my $verse_1 = "Why are there so many
+Questions of the hour";
+ 
+my $verse_2 = "Why are there so many
+Circumstances to be faced";
+ 
+my $verse_3 = "Who are we, and where are we going
+Where is the hope that we had before";
+ 
+my $verse_4 = "When we know that time is passing
+Seems like all we can do is wait";
+ 
+my $verse_5 = "Maybe when we find the answer
+We will know peace in the end";
+ 
+my $verse_6 = "Why are there rainbows in the sky
+When all the colors are blended";
+ 
+my $verse_7 = "If you think that you can find
+The Rainbow Connection";
+ 
+my $verse_8 = "You know it's just around the bend";
+ 
+my $verse_9 = "Who said the road's not for wondering
+Where dreams may be made or broken";
+ 
+my $verse_10 = "The promises we make and the lies we tell
+To get what we want and what we need";
+ 
+my $verse_11 = "The secrets we keep and the secrets we share
+Are the things that keep us from being free";
+ 
+my $verse_12 = "Maybe when we find the answer
+We will break the chains of fear";
+ 
+my $verse_13 = "Why are there rainbows in the sky
+To show us that something better's near";
+ 
+my $verse_14 = "When you think that you can find
+The Rainbow Connection";
+ 
+my $verse_15 = "You know it's just around the bend";
+ 
+my $verse_16 = "The stars up in the Heavens
+Seem to know the dreams that we are seeking";
+ 
+my $verse_17 = " We'd like to believe that
+The rainbow that we're chasing
+Will lead us where we ought to be";
+ 
+my $verse_18 = "Maybe when we find the answer
+We will build the bridge of trust";
+ 
+my $verse_19 = "Why are there rainbows in the sky
+Just to remind us that it must";
+ 
+my $verse_20 = "That you can find
+The Rainbow Connection";
+ 
+my $verse_21 = "You know it's just around the bend";
+ 
+my $verse_22 = "We've been searching high and low
+To bridge the gaps that we find in life";
+ 
+my $verse_23 = "Maybe when we find the answer
+We won't have to deal with strife";
+ 
+my $verse_24 = "Why are there rainbows in the sky
+For all to admire";
+ 
+my $verse_25 = "If you think that you can find
+The Rainbow Connection";
+ 
+my $verse_26 = "You know it's just around the bend";
+ 
+my $verse_27 = "We've been searching high and low
+To try to understand our place in life";
+ 
+my $verse_28 = "Maybe when we find the answer
+We can find peace in our strife";
+ 
+my $verse_29 = "Why are there rainbows in the sky
+For all to behold";
+ 
+my $verse_30 = "If you think that you can find
+The Rainbow Connection";
+ 
+my $verse_31 = "You know it's just around the bend";
+ 
+my $verse_32 = "Maybe when we realize the beauty
+Of the things that lay in store";
+ 
+my $verse_33 = "We won't have to discover
+The Rainbow Connection anymore";
+ 
+my $verse_34 = "Why are there rainbows in the sky
+For all to enjoy";
+ 
+my $verse_35 = "If you think that you can find
+The Rainbow Connection";
+ 
+my $verse_36 = "You know it's just around the bend";
+ 
+my $verse_37 = "Let the fire in your heart burn bright
+Let your dreams come alive";
+ 
+my $verse_38 = "Maybe when we find the answer
+We will see the world through rainbow eyes";
+ 
+my $verse_39 = "Why are there rainbows in the sky
+To brighten up the night";
+ 
+my $verse_40 = "If you think that you can find
+The Rainbow Connection";
+ 
+my $verse_41 = "You know it's just around the bend";
+ 
+my $verse_42 = "We must never lose hope
+For what we can create";
+ 
+my $verse_43 = "Maybe when we find the answer
+We will create our own fate";
+ 
+my $verse_44 = "Why are there rainbows in the sky
+A glimpse of our destiny";
+ 
+my $verse_45 = "If you think that you can find
+The Rainbow Connection";
+ 
+my $verse_46 = "You know it's just around the bend";
+ 
+my $verse_47 = "And those places we go
+Where our dreams can be made real";
+ 
+my $verse_48 = "Maybe when we find the answer
+We won't have to hide how we feel";
+ 
+my $verse_49 = "Why are there rainbows in the sky
+A sign that beauty will endure";
+ 
+my $verse_50 = "If you think that you can find
+The Rainbow Connection";
+ 
+my $verse_51 = "You know it's just around the bend";
+ 
+my $verse_52 = "The search that we go through
+Helps us build our strength and pride";
+ 
+my $verse_53 = "Maybe when we find the answer
+We can move forward side by side";
+ 
+my $verse_54 = "Why are there rainbows in the sky
+Linking us all in unity";
+ 
+my $verse_55 = "If you think that you can find
+The Rainbow Connection";
+ 
+my $verse_56 = "You know it's just around the bend";
+ 
+my $verse_57 = "When we finally find the answer
+Let us never look back again";
+ 
+my $verse_58 = "The beauty that we find in life
+Is the hope that will never end";
+ 
+my $verse_59 = "Why are there rainbows in the sky
+Always guiding us home";
+ 
+my $verse_60 = "If you think that you can find
+The Rainbow Connection";
+ 
+my $verse_61 = "You know it's just around the bend";
+ 
+print "The Rainbow Connection\n\n";
+print "$verse_1\n";
+print "$verse_2\n";
+print "$verse_3\n";
+print "$verse_4\n";
+print "$verse_5\n";
+print "$verse_6\n";
+print "$verse_7\n";
+print "$verse_8\n";
+print "$verse_9\n";
+print "$verse_10\n";
+print "$verse_11\n";
+print "$verse_12\n";
+print "$verse_13\n";
+print "$verse_14\n";
+print "$verse_15\n";
+print "$verse_16\n";
+print "$verse_17\n";
+print "$verse_18\n";
+print "$verse_19\n";
+print "$verse_20\n";
+print "$verse_21\n";
+print "$verse_22\n";
+print "$verse_23\n";
+print "$verse_24\n";
+print "$verse_25\n";
+print "$verse_26\n";
+print "$verse_27\n";
+print "$verse_28\n";
+print "$verse_29\n";
+print "$verse_30\n";
+print "$verse_31\n";
+print "$verse_32\n";
+print "$verse_33\n";
+print "$verse_34\n";
+print "$verse_35\n";
+print "$verse_36\n";
+print "$verse_37\n";
+print "$verse_38\n";
+print "$verse_39\n";
+print "$verse_40\n";
+print "$verse_41\n";
+print "$verse_42\n";
+print "$verse_43\n";
+print "$verse_44\n";
+print "$verse_45\n";
+print "$verse_46\n";
+print "$verse_47\n";
+print "$verse_48\n";
+print "$verse_49\n";
+print "$verse_50\n";
+print "$verse_51\n";
+print "$verse_52\n";
+print "$verse_53\n";
+print "$verse_54\n";
+print "$verse_55\n";
+print "$verse_56\n";
+print "$verse_57\n";
+print "$verse_58\n";
+print "$verse_59\n";
+print "$verse_60\n";
+print "$verse_61\n";
