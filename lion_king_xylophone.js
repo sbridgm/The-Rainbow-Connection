e84032c2afd62d6f4de2c380cad52b68c1961519
sbridgm <136629639+sbridgm@users.noreply.github.com>
2023-06-28 01:27:17 +0700

    Resolve compatibility issues

---
+++ lion_king_xylophone.js
@@ -0,0 +1,7 @@
+var rainbowConnection = "Why are there so many songs about rainbows, and what's on the other side? ";
+
+for (var x = 0; x < 2000; x++) {
+  rainbowConnection = rainbowConnection + "Rainbows are visions, but only illusions. And rainbows have nothing to hide. ";
+}
+
+console.log(rainbowConnection);
