6479744c399135d9e16aff71d821089a93f97303
sbridgm <136629639+sbridgm@users.noreply.github.com>
2023-07-19 20:44:31 +0700

    Implement feature request

---
+++ elephant_river_garden.py
@@ -0,0 +1,201 @@
+def rainbow_conn(message): 
+    conn = "Someday we'll find it, the rainbow connection- " + message  
+    return conn 
+  
+def goal(): 
+    print("The goal of life is to seek connection - connection to ourselves, and the world around us") 
+
+  
+def main(): 
+    print(rainbow_conn("the lovers, the dreamers, and me")) 
+    goal() 
+  
+if __name__ == "__main__": 
+    main() 
+
+#-------------------------------------
+
+def rainbow_conn(message): 
+    conn = "Why are there so many songs about rainbows - " + message  
+    return conn 
+  
+def freedom(): 
+    print("Commanding our own destiny, and having the freedom to choose") 
+
+  
+def main(): 
+    print(rainbow_conn("and what's on the other side?")) 
+    freedom() 
+  
+if __name__ == "__main__": 
+    main() 
+
+#-------------------------------------
+
+def rainbow_conn(message): 
+    conn = "Rainbows are visions, but only illusions - " + message  
+    return conn 
+  
+def reach(): 
+    print("Reaching out beyond the limits and borders of life and death") 
+
+  
+def main(): 
+    print(rainbow_conn("and rainbows have nothing to hide")) 
+    reach() 
+  
+if __name__ == "__main__": 
+    main() 
+
+#-------------------------------------
+
+def rainbow_conn(message): 
+    conn = "In search of the answer, it's so hard to find - " + message  
+    return conn 
+  
+def things(): 
+    print("In search of the things we can only find within our own mind") 
+
+  
+def main(): 
+    print(rainbow_conn("and it's getting harder all the time")) 
+    things() 
+  
+if __name__ == "__main__": 
+    main() 
+
+#-------------------------------------
+
+def rainbow_conn(message): 
+    conn = "It's a bridge to a place, where we all want to be - " + message  
+    return conn 
+  
+def hope(): 
+    print("No more lies, no more fear, a place of hope and understanding") 
+
+  
+def main(): 
+    print(rainbow_conn("somewhere we know that love will lead the way")) 
+    hope() 
+  
+if __name__ == "__main__": 
+    main() 
+
+#-------------------------------------
+
+def rainbow_conn(message): 
+    conn = "Let's keep believing, dreamers never die - " + message  
+    return conn 
+  
+def keep(): 
+    print("No, we never will, no, we never will, keep believing") 
+
+  
+def main(): 
+    print(rainbow_conn("in the promises of yesterday")) 
+    keep() 
+  
+if __name__ == "__main__": 
+    main() 
+
+#-------------------------------------
+
+def rainbow_conn(message): 
+    conn = "It's the mystery of life, that brings us the joy - " + message  
+    return conn 
+  
+def living(): 
+    print("Living in perfect harmony, in a world free of pain and sorrow") 
+
+  
+def main(): 
+    print(rainbow_conn("of imagining what our future can hold")) 
+    living() 
+  
+if __name__ == "__main__": 
+    main() 
+
+#-------------------------------------
+
+def rainbow_conn(message): 
+    conn = "Oh, we're all on our journey, and each one of us knows - " + message  
+    return conn 
+  
+def purpose(): 
+    print("Finding our purpose in life, and creating our own destiny") 
+
+  
+def main(): 
+    print(rainbow_conn("that the secrets of life, are never really told")) 
+    purpose() 
+  
+if __name__ == "__main__": 
+    main() 
+
+#-------------------------------------
+
+def rainbow_conn(message): 
+    conn = "The beauty of life, it's there and it's real - " + message  
+    return conn 
+  
+def journey(): 
+    print("Exploring our journey through life, and discovering our own truth") 
+
+  
+def main(): 
+    print(rainbow_conn("so it's time for us to rise and find the way")) 
+    journey() 
+  
+if __name__ == "__main__": 
+    main() 
+
+#-------------------------------------
+
+def rainbow_conn(message): 
+    conn = "We might be different, but different is real - " + message  
+    return conn 
+  
+def real(): 
+    print("Our differences give us strength and a will to search for the real") 
+
+  
+def main(): 
+    print(rainbow_conn("so let's keep looking for the rainbow connection")) 
+    real() 
+  
+if __name__ == "__main__": 
+    main() 
+
+#-------------------------------------
+
+def rainbow_conn(message): 
+    conn = "The rainbow connection, it's just within reach - " + message  
+    return conn 
+  
+def peace(): 
+    print("Sharing our love and peace, and learning to forgive our mistakes") 
+
+  
+def main(): 
+    print(rainbow_conn("just one step closer to finding inner peace")) 
+    peace() 
+  
+if __name__ == "__main__": 
+    main() 
+
+#-------------------------------------
+
+def rainbow_conn(message): 
+    conn = "And if we keep believing, then nothing can stand in our way - " + message  
+    return conn 
+  
+def dreams(): 
+    print("Dreaming our dreams, and believing that we can make them come true") 
+
+  
+def main(): 
+    print(rainbow_conn("the dream that we seek, will soon become reality")) 
+    dreams() 
+  
+if __name__ == "__main__": 
+    main()
