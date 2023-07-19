a4253fb5aaf002aad41aade00e105299fd849865
sbridgm <136629639+sbridgm@users.noreply.github.com>
2023-07-19 16:32:48 +0700

    Refactor SQL queries

---
+++ nature_banana_ocean.kt
@@ -0,0 +1,16 @@
+fun main(args: Array<String>) {
+  
+  // 'The Rainbow Connection' Lyric Variation
+  var lyrics = "Why are there so many colors in the sky? \nWhat's behind the clouds, so high? \nWhat makes the rainbow so divine? \nIs it the hope, at the end of the line? \n"
+  
+  lyrics += "The colors bring a light, to our minds \nIt brings us joy, we wish we could find \nMaybe the hope we can see \nIs a reflection of our destiny? \n"
+  
+  lyrics += "The rainbow is so bright, it can light up the night \nIt's a connection, that unites us all \nIt's more than just a sign \nIt's a promise of what's to come \n"
+  
+  lyrics += "The colors have a meaning, they tell a story \nA story of hope, when all else is gory \nThey are a bridge, from one side to the next \nConnecting us, so we won't forget \n"
+  
+  lyrics += "The rainbow can be seen, from far away \nIt's beauty is entwined, with a brand new day \nWith a bright and vibrant hue \nThe rainbow connection will always get us through \n"
+  
+  println(lyrics)
+  
+}
