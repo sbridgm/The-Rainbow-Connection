66350db46273c1bbb99ee91e13f4d0f912327158
sbridgm <136629639+sbridgm@users.noreply.github.com>
2023-07-20 14:46:09 +0700

    Add support for multiple languages

---
+++ river_house_zebra.rb
@@ -0,0 +1,47 @@
+# 'The Rainbow Connection'
+
+# Verse 1
+lyrics = "Why are there so many songs about rainbows \n
+And what's on the other side? \n
+Rainbows are visions, but only illusions \n
+And rainbows have nothing to hide \n"
+
+# Chorus
+lyrics += "So we've been told and some choose to believe it \n
+I know they're wrong, wait and see \n
+Some day we'll find it, the rainbow connection \n
+The lovers, the dreamers and me. \n"
+
+# Verse 2
+lyrics += "Who said that every wish would be heard and answered \n
+When wished on the morning star? \n
+Somebody thought of that and someone believed it \n
+Look what it's done so far \n"
+
+# Chorus
+lyrics += "What's so amazing that keeps us stargazing \n
+And what do we think we might see \n
+Some day we'll find it, the rainbow connection \n
+The lovers, the dreamers and me \n"
+
+# Verse 3
+lyrics += "Have you been half asleep and have you heard voices? \n
+I've heard them calling my name \n
+Is this the sweet sound that calls the young sailor? \n
+The voice might be one and the same \n"
+
+# Chorus
+lyrics += "It's called the " + '"Rainbow Connection" ' + "the lovers, the dreamers and me \n
+It's hope in a world that we can't comprehend \n
+Why are there so many songs about rainbows \n
+And what's on the other side? \n
+Rainbows are visions, but only illusions, \n
+And rainbows have nothing to hide \n"
+
+# Ending
+lyrics += "So we've been told and some choose to believe it \n
+I know they're wrong, wait and see \n
+Some day we'll find it, the rainbow connection \n
+The lovers, the dreamers and me \n"
+
+puts lyrics
