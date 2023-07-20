8562217189d5585ea50f7b718fec118300fda8a4
sbridgm <136629639+sbridgm@users.noreply.github.com>
2023-07-20 18:51:49 +0700

    Refactor SQL queries

---
+++ piano_waterfall_queen.scala
@@ -0,0 +1,45 @@
+object TheRainbowConnection {
+
+  def main(args: Array[String]): Unit = {
+
+    // Print the lyrics to "The Rainbow Connection"
+    println("Why are there so many songs about rainbows")
+    println("And what's on the other side")
+    println("Rainbows are visions, but only illusions")
+    println("And rainbows have nothing to hide")
+    println("So we've been told, and some choose to believe it")
+    println("I know they're wrong wait and see")
+    println("Some day we'll find it, the rainbow connection")
+    println("The lovers, the dreamers and me")
+
+    println("Who said that every wish would be heard and answered")
+    println("When wished on the morning star")
+    println("Somebody thought of that, and someone believed it")
+    println("Look what it's done so far")
+    println("What's so amazing that keeps us stargazing")
+    println("And what do we think we might see")
+    println("Some day we'll find it, the rainbow connection")
+    println("The lovers, the dreamers and me")
+
+    println("All of us under its spell, we know that it's probably magic")
+    println("Have you been half asleep, and have you heard voices")
+    println("I've heard them calling my name")
+    println("Is this the sweet sound that calls the young sailors")
+    println("The voice might be one and the same")
+    println("I've heard it too many times to ignore it")
+    println("It's something that I'm supposed to be")
+    println("Some day we'll find it, the rainbow connection")
+    println("The lovers, the dreamers and me")
+
+    println("La, la, la, la, la, la, la, la, la, la, la, la")
+
+    println("Why are there so many songs about rainbows")
+    println("And what's on the other side")
+    println("Rainbows are visions, but only illusions")
+    println("And rainbows have nothing to hide")
+    println("So we've been told, and some choose to believe it")
+    println("I know they're wrong wait and see")
+    println("Some day we'll find it, the rainbow connection")
+    println("The lovers, the dreamers and me")
+  }
+}
